23a957cdfd12caab25c107d61483a637cde5ef04
MyNameIsNeXTSTEP <madmath@bk.ru>
2024-12-12 15:10:57 +0300

    Fix: disable brew updates in import.sh

--- import.sh
+++ import.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-brew bundle dump --force --file=~/.brewfile && cp ~/.brewfile .brewfile
+HOMEBREW_NO_AUTO_UPDATE=1 brew bundle dump --force --file=~/.brewfile && cp ~/.brewfile .brewfile
 cp ~/.bashrc .bashrc
 cp ~/.tmux.conf .tmux.conf
 rm -drf .config && mkdir .config && cp -r ~/.config/nvim .config/nvim
