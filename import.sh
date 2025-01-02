7d503fb0ecb522bd9791f3743c7784a605d88a53
MyNameIsNeXTSTEP <madmath@bk.ru>
2025-01-02 14:44:41 +0300

    Comments on import file steps

--- import.sh
+++ import.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-HOMEBREW_NO_AUTO_UPDATE=1 brew bundle dump --force --file=~/.brewfile && cp ~/.brewfile .brewfile
-cp ~/.bashrc .bashrc
-cp ~/.tmux.conf .tmux.conf
-rm -drf .config && mkdir .config && cp -r ~/.config/nvim .config/nvim
+HOMEBREW_NO_AUTO_UPDATE=1 brew bundle dump --force --file=~/.brewfile && cp ~/.brewfile .brewfile # update brewfile
+cp ~/.bashrc .bashrc                                                                              # update bashrc file
+cp ~/.tmux.conf .tmux.conf                                                                        # update tmux config
+rm -drf .config && mkdir .config && cp -r ~/.config/nvim .config/nvim                             # update nvim config
